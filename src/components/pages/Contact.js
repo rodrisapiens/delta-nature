baf87dd8da33c781e088aa17fa46152c6608a463
rodrisapiens <rniveyroreguero@frba.utn.edu.ar>
2022-03-25 22:39:27 -0300

    htlm and css for contact page almost done

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import "../../styles/contact.css"
-import {ReactComponent as Birds} from "../../images/contactBirds.svg"
+import { ReactComponent as Birds } from "../../images/contactBirds.svg"
 function Contact() {
-  return (
-    <div className='contactPage'>
-        <Birds className='contactBirds'/>
-        <div className="contactConteiner">
-        <h1 className="enviame">Enviame un mensaje</h1>
-        <div className="contactBox">
-
+    return (
+        <div className='contactPage'>
+            <Birds className='contactBirds' />
+            <div className="contactConteiner">
+                <h1 className="enviame">Enviame un mensaje</h1>
+                <div className="contactBox">
+                    <div className="fill">
+                        <input type="text" className="contactFill" />
+                        <input type="email" className="contactFill" />
+                        <input type="text" className="contactFill" />
+                    </div>
+                    <textarea className='contactMessage'/>
+                </div>
+                <button className="more big">SEND</button>
+            </div>
         </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Contact
