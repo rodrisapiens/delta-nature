7fbed8ca0bf22f1a3bc3179f4ebff9bee7f3807d
rodrisapiens <rniveyroreguero@frba.utn.edu.ar>
2022-03-29 04:28:25 -0300

    sgfsfd

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import "../../styles/aboutUs.css";
 import { aboutMe } from "../../api";
 function AboutUs() {
+  useEffect(() => {
+    window.scroll(0,0)
+  }, [])
+  
   return (
     <div className='aboutUs'>
       <div className="firstAboutPage">
